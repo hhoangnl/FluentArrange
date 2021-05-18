d8d23c8f4f9e6475016492bba53e05cf1b73e0f4
Huy <10050270+hhoangnl@users.noreply.github.com>
2021-05-18 19:19:50 +0200

    Added extension methods for cleaner arrange code. (#13)
    Added extension methods for cleaner arrange code

---
+++ FluentArrange.Tests/ExtensionMethods.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using FluentArrange.Tests.TestClasses;
+using FluentAssertions;
+using Xunit;
+
+namespace FluentArrange.Tests
+{
+    public class ExtensionMethods
+    {
+        [Fact]
+        public void Returns_ActionT_ShouldInvokeAction()
+        {
+            // Arrange
+            Foo foo = new();
+
+            // Act
+            foo.Returns(e => e.Id = 1);
+
+            // Assert
+            foo.Id.Should().Be(1);
+        }
+
+        [Fact]
+        public void Returns_ActionTaskT_ShouldInvokeAction()
+        {
+            // Arrange
+            Task<Foo> foo = Task.FromResult(new Foo());
+
+            // Act
+            foo.Returns(e => e.Id = 2);
+
+            // Assert
+            foo.Result.Id.Should().Be(2);
+        }
+    }
+}
