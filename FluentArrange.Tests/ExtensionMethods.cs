5054569371420e8da0e5db2f258f0db090e7409c
Huy <10050270+hhoangnl@users.noreply.github.com>
2021-05-24 18:37:25 +0200

    Added new extension method `With` as replacement for `Return`. (#14)
    Added new extension method `With` as replacement for `Return`
    Marked extension method `Return` as deprecated.

--- FluentArrange.Tests/ExtensionMethods.cs
+++ FluentArrange.Tests/ExtensionMethods.cs
@@ -32,5 +32,44 @@ namespace FluentArrange.Tests
             // Assert
             foo.Result.Id.Should().Be(2);
         }
+
+        [Fact]
+        public void With_ActionT_ShouldInvokeAction()
+        {
+            // Arrange
+            Foo foo = new();
+
+            // Act
+            foo.With(e => e.Id = 1);
+
+            // Assert
+            foo.Id.Should().Be(1);
+        }
+
+        [Fact]
+        public void With_ActionTaskT_ShouldInvokeAction()
+        {
+            // Arrange
+            Task<Foo> foo = Task.FromResult(new Foo());
+
+            // Act
+            foo.With(e => e.Id = 2);
+
+            // Assert
+            foo.Result.Id.Should().Be(2);
+        }
+
+        [Fact]
+        public void With_ShouldAlways_ReturnItself()
+        {
+            // Arrange
+            var foo = new Foo();
+
+            // Act
+            var result = foo.With(e => e.Id = 3);
+
+            // Assert
+            result.Should().Be(foo);
+        }
     }
 }
