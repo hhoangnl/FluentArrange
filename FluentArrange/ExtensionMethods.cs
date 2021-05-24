a2ba7269e22e5b136089d6e2541935bfed6d8917
Huy Hoang <10050270+hhoangnl@users.noreply.github.com>
2021-05-24 18:34:26 +0200

    Added new extension method `With` as replacement for `Return`. Marked extension method `Return` as deprecated.

--- FluentArrange/ExtensionMethods.cs
+++ FluentArrange/ExtensionMethods.cs
@@ -5,14 +5,28 @@ namespace FluentArrange
 {
     public static class ExtensionMethods
     {
+        [Obsolete("Please use the extension method With instead")]
         public static void Returns<T>(this Task<T> @object, Action<T> configure)
         {
             configure.Invoke(@object.GetAwaiter().GetResult());
         }
 
+        [Obsolete("Please use the extension method With instead")]
         public static void Returns<T>(this T @object, Action<T> configure)
         {
             configure.Invoke(@object);
+        }
+
+        public static Task<T> With<T>(this Task<T> @object, Action<T> configure)
+        {
+            configure.Invoke(@object.GetAwaiter().GetResult());
+            return @object;
+        }
+
+        public static T With<T>(this T @object, Action<T> configure)
+        {
+            configure.Invoke(@object);
+            return @object;
         }
     }
 }
