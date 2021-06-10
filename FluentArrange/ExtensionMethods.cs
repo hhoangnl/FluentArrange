5bb7ab1fee4f3761c1af3b0fdff4c79c2724014a
Huy Hoang <dev@huyhoang.nl>
2021-06-10 20:07:18 +0200

    Removed obsolete code

--- FluentArrange/ExtensionMethods.cs
+++ FluentArrange/ExtensionMethods.cs
@@ -5,18 +5,6 @@ namespace FluentArrange
 {
     public static class ExtensionMethods
     {
-        [Obsolete("Please use the extension method With instead")]
-        public static void Returns<T>(this Task<T> @object, Action<T> configure)
-        {
-            configure.Invoke(@object.GetAwaiter().GetResult());
-        }
-
-        [Obsolete("Please use the extension method With instead")]
-        public static void Returns<T>(this T @object, Action<T> configure)
-        {
-            configure.Invoke(@object);
-        }
-
         public static Task<T> With<T>(this Task<T> @object, Action<T> configure)
         {
             configure.Invoke(@object.GetAwaiter().GetResult());
