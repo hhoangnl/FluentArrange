b33b0c4f47f8ca5092e5459baf0e993c4c6b74dc
Huy <hhoang.nl@gmail.com>
2021-06-10 20:13:44 +0200

    Removed obsolete code (#15)

--- FluentArrange.Tests/ExtensionMethods.cs
+++ FluentArrange.Tests/ExtensionMethods.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿// Copyright (c) Huy Hoang. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System.Threading.Tasks;
 using FluentArrange.Tests.TestClasses;
 using FluentAssertions;
 using Xunit;
@@ -7,32 +10,6 @@ namespace FluentArrange.Tests
 {
     public class ExtensionMethods
     {
-        [Fact]
-        public void Returns_ActionT_ShouldInvokeAction()
-        {
-            // Arrange
-            Foo foo = new();
-
-            // Act
-            foo.Returns(e => e.Id = 1);
-
-            // Assert
-            foo.Id.Should().Be(1);
-        }
-
-        [Fact]
-        public void Returns_ActionTaskT_ShouldInvokeAction()
-        {
-            // Arrange
-            Task<Foo> foo = Task.FromResult(new Foo());
-
-            // Act
-            foo.Returns(e => e.Id = 2);
-
-            // Assert
-            foo.Result.Id.Should().Be(2);
-        }
-
         [Fact]
         public void With_ActionT_ShouldInvokeAction()
         {
